0df1051536cde535ed75dc4c75fd176ed08f0b70
ThaiVoPro <tanthaipro1999@gmail.com>
2024-09-27 17:58:05 +0700

    update voucher, nhanvien be error

---
+++ src/main/java/com/dreammovie/voucher/VoucherRepository.java
@@ -0,0 +1,13 @@
+package com.dreammovie.voucher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VoucherRepository extends JpaRepository <Voucher, Long> {
+// JpaRepository đã cung cấp sẵn các thao tác CRUD.
+    List<Voucher> findByDeletedAtIsNull();
+//    Page<ChairCategory> findAll(Pageable pageable);
+}
